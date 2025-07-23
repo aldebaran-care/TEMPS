3852a40e114cf187ea3570ef7f2cf52f3cc89ea4
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-07-23 16:15:46 +0200

    Add cache to salesforce evaluation

--- temporal_embeddings/evaluation/utils/evaluation/salesforce/salesforce_evaluation.py
+++ temporal_embeddings/evaluation/utils/evaluation/salesforce/salesforce_evaluation.py
@@ -12,10 +12,10 @@ from temporal_embeddings.evaluation.utils.evaluation.metrics import compute_metr
 def get_detailed_instruct(task_description: str, query: str) -> str:
     return f'Instruct: {task_description}\nQuery: {query}'
 
-def evaluate_salesforce(dataset_file_path: Path, eval_id: int, top_k: int, metric: str, skip: bool) -> None:
-    GROUND_TRUTH_FILE_PATH: Path = dataset_file_path
+def evaluate_salesforce(benchmark_file_path: Path, eval_id: int, top_k: int, metric: str, skip: bool) -> None:
     model_name = "Salesforce/SFR-Embedding-Mistral"
-    SIMILARITIES_FILE_PATH: Path = Path(f"output/similarities/salesforce/{model_name}/{dataset_file_path.stem}/{eval_id}_similarities.json")
+    
+    SIMILARITIES_FILE_PATH: Path = Path(f"output/similarities/{benchmark_file_path.stem}/{model_name}/{eval_id}_similarities.json")
     create_folders(SIMILARITIES_FILE_PATH.parent)
 
     embeddings_cache = pd.DataFrame(columns=["text", "embedding"]).set_index("text")
@@ -25,23 +25,23 @@ def evaluate_salesforce(dataset_file_path: Path, eval_id: int, top_k: int, metri
 
         model = SentenceTransformer(model_name, trust_remote_code=True)
 
-        similarities_list: List[List[float]] = []
+        output_similarities: List[List[float]] = []
 
-        data: List[Dict] = []
+        benchmark_data: List[Dict] = []
 
         print(f"Evaluating model: {model_name}")
-        print(f"Dataset file path: {GROUND_TRUTH_FILE_PATH}")
-        
-        with GROUND_TRUTH_FILE_PATH.open("r", encoding="utf-8") as f:
-            data = json.load(f)
+        print(f"Dataset file path: {benchmark_file_path}")
+
+        with benchmark_file_path.open("r", encoding="utf-8") as f:
+            benchmark_data = json.load(f)
 
             unique_texts = set()
-            
-            for element in tqdm(data, desc="Collecting unique texts"):
-                question = get_detailed_instruct(task, element["question"])
+
+            for benchmark_item in tqdm(benchmark_data, desc="Collecting unique texts"):
+                question = get_detailed_instruct(task, benchmark_item["question"])
                 unique_texts.add(question)
-                unique_texts.update(element["paragraphs"])
-            
+                unique_texts.update(benchmark_item["paragraphs"])
+
             unique_texts = list(unique_texts)
             
             texts_to_encode = [t for t in unique_texts if t not in embeddings_cache.index]
@@ -52,7 +52,7 @@ def evaluate_salesforce(dataset_file_path: Path, eval_id: int, top_k: int, metri
                 for t, emb in zip(texts_to_encode, new_embeddings):
                     embeddings_cache.loc[t] = [emb]
 
-            for element in tqdm(data, desc="Evaluating"):
+            for element in tqdm(benchmark_data, desc="Evaluating"):
                 question: str = get_detailed_instruct(task, element["question"])
                 paragraphs: List[str] = element["paragraphs"]
 
@@ -68,17 +68,17 @@ def evaluate_salesforce(dataset_file_path: Path, eval_id: int, top_k: int, metri
                     scores = util.cos_sim(embeddings[0], embeddings[i+1])
                     similarities.append(scores.tolist()[0][0])
 
-                similarities_list.append(similarities)
-        
+                output_similarities.append(similarities)
+
         with SIMILARITIES_FILE_PATH.open("w", encoding="utf-8") as g:
-            json.dump(similarities_list, g, indent=4, ensure_ascii=False)
+            json.dump(output_similarities, g, indent=4, ensure_ascii=False)
 
     ground_truth: List[List[int]] = []
 
-    with GROUND_TRUTH_FILE_PATH.open("r", encoding="utf-8") as f:
-        data: List[Dict] = json.load(f)
+    with benchmark_file_path.open("r", encoding="utf-8") as f:
+        benchmark_data: List[Dict] = json.load(f)
 
-        for element in data:
+        for element in benchmark_data:
             ground_truth.append(element["answer"])
 
     output_similarities: List[List[float]] = []
