de7c5c8cb25fa1893009a206959eab81f88b8476
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-07-25 23:46:37 +0200

    Train on entire dataset

--- train.sh
+++ train.sh
@@ -6,5 +6,5 @@
 source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
 conda activate train-env
-python3 train.py --data_fraction=0.5 --epochs=1 --batch_size=128 --model_name=sentence-transformers/all-MiniLM-L6-v2 --input_file_path=data/dataset/full_dataset.csv
+python3 train.py --data_fraction=1.0 --epochs=1 --batch_size=128 --model_name=sentence-transformers/all-MiniLM-L6-v2 --input_file_path=data/dataset/full_dataset.csv
 conda deactivate
