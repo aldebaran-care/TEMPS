892b15d61beb7970a9adf2665bf0c70b44f117f3
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-07-31 10:50:29 +0200

    TS_Retriever: all paragraphs are candidates

--- temporal_embeddings/evaluation/utils/evaluation/temporal_bert_full/temporal_bert_full.py
+++ temporal_embeddings/evaluation/utils/evaluation/temporal_bert_full/temporal_bert_full.py
@@ -19,6 +19,11 @@ def evaluate_temporal_bert_full(model_name: str, external_model_name: str, model
     create_folders(EXTERNAL_SIMILARITIES_FILE_PATH.parent)
 
     def run_temporal_bert(model_name: str, model_path: Path, batch_size: int, max_seq_len: int) -> None:
+        if "ts_retriever" in benchmark_file_path:
+            ts_retriever_paragraphs: List[str] = []
+            with Path("data/evaluation/ts_retriever/doc.json").open("r", encoding="utf-8") as f:
+                ts_retriever_paragraphs = json.load(f)
+
         output_similarities: List[List[float]] = []
 
         reference_date: str = "09 august 2024"
@@ -31,7 +36,7 @@ def evaluate_temporal_bert_full(model_name: str, external_model_name: str, model
             for benchmark_item in tqdm(benchmark_data):
                 question: str = benchmark_item["question"]
 
-                paragraphs: List[str] = benchmark_item["paragraphs"]
+                paragraphs: List[str] = benchmark_item["paragraphs"] if "ts_retriever" not in benchmark_file_path else ts_retriever_paragraphs
                 questions: List[str] = [question] * len(paragraphs)
                 reference_dates: List[str] = [reference_date] * len(paragraphs)
                 ground_truth: List[float] = [0.0] * len(paragraphs)
