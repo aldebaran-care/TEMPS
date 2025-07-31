9b482ea5a02f7fddf1a3055d53b7a6c7e96e3f32
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-07-31 16:30:08 +0200

    Bug fix

--- temporal_embeddings/evaluation/utils/evaluate_sentence_transformer.py
+++ temporal_embeddings/evaluation/utils/evaluate_sentence_transformer.py
@@ -60,7 +60,7 @@ def evaluate_sentence_transformer(model_name: str, max_seq_len: int, benchmark_f
                     else:
                         paragraph_emb = embedding_cache.loc[paragraph, 'embedding']
 
-                    similarities.append(float(util.cos_sim(question_emb, paragraph_emb)[0].item()))
+                    similarities.append(float(util.cos_sim(question_emb.cpu(), paragraph_emb.cpu())[0].item()))
 
                 output_similarities.append(similarities)
 
