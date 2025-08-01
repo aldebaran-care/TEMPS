052e7da9b253ce3c6aa456f75a8c2da13bc74f8b
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-08-01 17:05:32 +0200

    Bug fix

--- temporal_embeddings/model/gauss_model.py
+++ temporal_embeddings/model/gauss_model.py
@@ -46,6 +46,6 @@ class GaussModel(nn.Module):
     def mean_pooling(self, model_output, attention_mask):
         # token_embeddings = model_output[0] #First element of model_output contains all token embeddings
         token_embeddings = model_output.last_hidden_state
-        attention_mask_copy = attention_mask.copy()
+        attention_mask_copy = attention_mask.clone()
         input_mask_expanded = attention_mask_copy.unsqueeze(-1).repeat(1, 1, token_embeddings.size(-1))
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
