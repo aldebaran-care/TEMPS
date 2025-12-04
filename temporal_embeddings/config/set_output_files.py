274e40311577ff0aaab53fd7bf6268ca7099e18c
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-12-04 17:04:20 +0100

    Bug fix

--- temporal_embeddings/config/set_output_files.py
+++ temporal_embeddings/config/set_output_files.py
@@ -26,27 +26,27 @@ def set_output_files(temporal_model_name: str, temporal_model_path: Path, semant
 
     output_paths = config.get("output_paths", {})
 
-    temporal_similarities_path = output_paths["temporal_similarities_path"].format(
+    temporal_similarities_path = output_paths["temporal_similarities_path"]["default"].format(
         benchmark_stem=benchmark_stem,
         model_name=temporal_model_name,
         model_stem=temporal_model_path.stem,
         eval_id=eval_id
     )
 
-    semantic_similarities_path = output_paths["semantic_similarities_path"].format(
+    semantic_similarities_path = output_paths["semantic_similarities_path"]["default"].format(
         benchmark_stem=benchmark_stem,
         external_model_name=semantic_model_name,
         eval_id=eval_id
     )
 
-    temporal_cache_path = output_paths["temporal_cache_path"].format(
+    temporal_cache_path = output_paths["temporal_cache_path"]["default"].format(
         benchmark_stem=benchmark_stem,
         model_name=temporal_model_name,
         model_stem=temporal_model_path.stem,
         eval_id=eval_id
     )
 
-    semantic_cache_path = output_paths["semantic_cache_path"].format(
+    semantic_cache_path = output_paths["semantic_cache_path"]["default"].format(
         benchmark_stem=benchmark_stem,
         external_model_name=semantic_model_name,
         eval_id=eval_id
