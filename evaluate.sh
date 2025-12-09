83c172720917fabaeb3a0f6745ea83a76550b245
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-12-09 12:43:35 +0100

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -28,7 +28,7 @@ for alpha in "${ALPHA_VALUES[@]}"; do
             
             for external_model in "${EXTERNAL_MODELS[@]}"; do
                 echo "External Model: $external_model"
-                python3 evaluate.py --model_name=all-minilm-l6-v2-full --external_model_name=$external_model --model_path=output/trained_models/model_sentence-transformers_all-MiniLM-L6-v2_2025-07-25_23-47-04.pth --batch_size=128 --max_seq_len=512 --benchmark=$benchmark --eval_id=2 --top_k=$top_k --metric=all --alpha=$alpha --use_all_paragraphs | grep "^{'top':"
+                python3 evaluate.py --model_name=all-minilm-l6-v2-full --external_model_name=$external_model --model_path=output/trained_models/model_sentence-transformers_all-MiniLM-L6-v2_2025-07-25_23-47-04.pth --batch_size=128 --max_seq_len=512 --benchmark=$benchmark --eval_id=1 --top_k=$top_k --metric=all --alpha=$alpha --use_all_paragraphs | grep "^{'top':"
             done
         done
     done
