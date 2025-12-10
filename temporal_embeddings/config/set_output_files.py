9c151d8e843e7acd04738a78aa4e6151cde8f3e5
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-12-10 21:11:49 +0100

    Remove id from output paths

--- temporal_embeddings/config/set_output_files.py
+++ temporal_embeddings/config/set_output_files.py
@@ -4,21 +4,7 @@ import json
 
 from temporal_embeddings.utils.os.folder_management import create_folders
 
-def set_output_files(temporal_model_name: str, temporal_model_path: Path, semantic_model_name: str, benchmark: str, eval_id: int) -> Dict:
-    """
-    Set the output file paths in the configuration dictionary based on the provided parameters.
-
-    Args:
-        benchmark_file_path (Path): Path to the benchmark file.
-        model_name (str): Name of the temporal model.
-        model_stem (str): Stem of the temporal model file.
-        external_model_name (str): Name of the external model.
-        eval_id (int): Evaluation ID to identify the experiment.
-        config (dict): Configuration dictionary to update.
-
-    Returns:
-        dict: Updated configuration dictionary with output file paths set.
-    """
+def set_output_files(temporal_model_name: str, temporal_model_path: Path, semantic_model_name: str, benchmark: str) -> Dict:
     if temporal_model_name.endswith("-full"):
         temporal_model_name = temporal_model_name[:-5]
 
@@ -30,27 +16,23 @@ def set_output_files(temporal_model_name: str, temporal_model_path: Path, semant
     temporal_similarities_path = Path(output_paths["temporal_similarities_path"]["default"].format(
         benchmark_stem=benchmark,
         model_name=temporal_model_name,
-        model_stem=temporal_model_path.stem,
-        eval_id=eval_id
+        model_stem=temporal_model_path.stem
     ))
 
     semantic_similarities_path = Path(output_paths["semantic_similarities_path"]["default"].format(
         benchmark_stem=benchmark,
-        external_model_name=semantic_model_name,
-        eval_id=eval_id
+        external_model_name=semantic_model_name
     ))
 
     temporal_cache_path = Path(output_paths["temporal_cache_path"]["default"].format(
         benchmark_stem=benchmark,
         model_name=temporal_model_name,
-        model_stem=temporal_model_path.stem,
-        eval_id=eval_id
+        model_stem=temporal_model_path.stem
     ))
 
     semantic_cache_path = Path(output_paths["semantic_cache_path"]["default"].format(
         benchmark_stem=benchmark,
-        external_model_name=semantic_model_name,
-        eval_id=eval_id
+        external_model_name=semantic_model_name
     ))
 
     create_folders([temporal_similarities_path.parent,
