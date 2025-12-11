1dec98cda805a528e1d27a927ab0528b97ec4ed2
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-12-11 14:02:49 +0100

    Update evaluate.sh file

--- evaluate.sh
+++ evaluate.sh
@@ -7,11 +7,10 @@ source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
 conda activate train-env
 
-TOP_K_VALUES=(1 10)
-ALPHA_VALUES=(0.25 0.5 0.75)
 BENCHMARKS=("time_sensitive_qa" "ts_retriever" "temp_reason_l1")
 EXTERNAL_MODELS=("intfloat/e5-base-v2")
 NUM_NEGATIVE_SAMPLES=(-1 0 10 100)
+ALPHA_VALUES=(0.25 0.5 0.75)
 
 for alpha in "${ALPHA_VALUES[@]}"; do
     echo "##########################################"
@@ -32,7 +31,7 @@ for alpha in "${ALPHA_VALUES[@]}"; do
                 
                 for external_model in "${EXTERNAL_MODELS[@]}"; do
                     echo "External Model: $external_model"
-                    python3 evaluate.py --model_name=all-minilm-l6-v2-full --external_model_name=$external_model --model_path=output/trained_models/model_sentence-transformers_all-MiniLM-L6-v2_2025-07-25_23-47-04.pth --batch_size=128 --max_seq_len=512 --benchmark=$benchmark --eval_id=1 --top_k=$top_k --metric=all --alpha=$alpha --num_negative_samples=$num_neg | grep "^{'top':"
+                    python3 evaluate.py --model_name=all-minilm-l6-v2-full --external_model_name=$external_model --model_path=output/trained_models/model_sentence-transformers_all-MiniLM-L6-v2_2025-07-25_23-47-04.pth --batch_size=128 --max_seq_len=512 --benchmark=$benchmark --eval_id="test full model wiht k=10" --top_k=10 --metric=all --alpha=$alpha --num_negative_samples=$num_neg | grep "^{'top':"
                 done
             done
         done
