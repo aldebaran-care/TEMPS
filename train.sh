1b8a632707dd5e070aad7bebd411b33f6e054ec1
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-13 00:39:42 +0100

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --time=23:00:00
-#SBATCH --gres=gpu:4
-#SBATCH --nodelist=n101
+#SBATCH --gres=gpu:1
+#SBATCH --nodelist=n54
 
 source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
 conda activate train-env
-python3 train.py --data_fraction=1.0 --epochs=1 --batch_size=128 --model_name=sentence-transformers/all-MiniLM-L6-v2 --input_file_path=data/dataset/dataset.csv
+python3 train.py --data_fraction=0.1 --epochs=1 --batch_size=128 --model_name=prajjwal1/bert-tiny --input_file_path=data/new_training_dataset/synthetic_dataset/synthetic_dataset.csv
 conda deactivate
