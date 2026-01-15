9fe05154907abd0042c949e9274580c6ea9ac7a5
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-15 17:02:22 +0100

    Update train.sh

--- train.sh
+++ train.sh
@@ -6,5 +6,5 @@
 source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
 conda activate train-env
-python3 train.py --data_fraction=1 --epochs=1 --batch_size=128 --model_name=intfloat/e5-base-v2 --input_file_path=data/new_training_dataset/training_datasets/training_data_chunk_21_30.csv
+python3 train.py --data_fraction=1 --epochs=1 --batch_size=128 --model_name=intfloat/e5-base-v2 --input_file_path=data/new_training_dataset/training_datasets/training_data_chunk_1.csv
 conda deactivate
