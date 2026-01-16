dcaa86675e7dea9ffd781810986c58253edd7938
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-16 09:47:16 +0100

    Remove intfloat/e5-base-v2

--- temporal_embeddings/evaluation/evaluate.py
+++ temporal_embeddings/evaluation/evaluate.py
@@ -10,10 +10,10 @@ from temporal_embeddings.evaluation.utils.evaluation.sutime.evaluate_sutime impo
 def evaluate_model(model_name: str, external_model_name: str, model_path: Path, batch_size: int, max_seq_len: int, benchmark: str, eval_id: str, top_k: int, metric: str, alpha: float, num_negative_samples: int = 0) -> None:
     benchmark_file_path: Path = get_dataset_file_path(benchmark)
 
-    if model_name in ["temporal_bert", "all-minilm-l6-v2", "prajjwal1/bert-tiny", "intfloat/e5-base-v2"]:
+    if model_name in ["temporal_bert", "all-minilm-l6-v2", "prajjwal1/bert-tiny"]:
         evaluate_temporal_model(model_name, model_path, batch_size, max_seq_len, benchmark, benchmark_file_path, eval_id, top_k, metric, num_negative_samples)
     
-    elif model_name in ["temporal_bert_full", "all-minilm-l6-v2-full", "prajjwal1/bert-tiny-full", "intfloat/e5-base-v2-full"]:
+    elif model_name in ["temporal_bert_full", "all-minilm-l6-v2-full", "prajjwal1/bert-tiny-full"]:
         evaluate_temporal_semantic_model(model_name, external_model_name, model_path, batch_size, max_seq_len, benchmark, benchmark_file_path, eval_id, top_k, metric, alpha, num_negative_samples)
 
     elif model_name == "bm25":
