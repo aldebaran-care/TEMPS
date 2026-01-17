16f6ee96d23ad553716509176dce3195e86c9117
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-18 00:02:28 +0100

    Update menat_qa in create_evaluation_dataset.py

--- create_evaluation_dataset.py
+++ create_evaluation_dataset.py
@@ -1,89 +1,13 @@
-import json
-from pathlib import Path
 import argparse
-import random
-from typing import List
 
 from temporal_embeddings.evaluation.benchmarks.time_sensitive_qa import create_time_sensitive_qa_benchmark
 from temporal_embeddings.evaluation.benchmarks.ts_retriever import create_ts_retriever_benchmark
 from temporal_embeddings.evaluation.benchmarks.temp_reason_l1 import create_temp_reason_l1_benchmark
+from temporal_embeddings.evaluation.benchmarks.menat_qa import create_menat_qa_benchmark
 
 def create_evaluation_dataset(dataset_name):
     if dataset_name.lower().startswith("menat_qa"):
-        main_folder = Path("data/evaluation/menat_qa")
-        input_path = main_folder / Path("MenatQA.json")
-        
-        if dataset_name.lower() == "menat_qa":
-            output_file = main_folder / "processed_menat_qa.json"
-        
-        elif dataset_name.lower() == "menat_qa_granularity":
-            output_file = main_folder / "processed_menat_qa_granularity.json"
-        
-        elif dataset_name.lower() == "menat_qa_counterfactual":
-            output_file = main_folder / "processed_menat_qa_counterfactual.json"
-        
-        elif dataset_name.lower() == "menat_qa_expand":
-            output_file = main_folder / "processed_menat_qa_expand.json"
-        
-        elif dataset_name.lower() == "menat_qa_narrow":
-            output_file = main_folder / "processed_menat_qa_narrow.json"
-        
-        else:
-            print(f"Dataset '{dataset_name}' is not supported.")
-            return
-
-        main_folder.mkdir(parents=True, exist_ok=True)
-
-        with input_path.open("r", encoding="utf-8") as infile:
-            menat_data = json.load(infile)
-
-        processed_data = []
-
-        all_paragraphs = set([ctx["text"] for item in menat_data for ctx in item["context"]])
-        
-        for item in menat_data:
-            if dataset_name.lower() == "menat_qa_granularity" and item.get("type") != "granularity":
-                continue
-            
-            if dataset_name.lower() == "menat_qa_counterfactual" and item.get("type") != "counterfactual":
-                continue
-            
-            if dataset_name.lower() == "menat_qa_expand" and item.get("type") != "expand":
-                continue
-            
-            if dataset_name.lower() == "menat_qa_narrow" and item.get("type") != "narrow":
-                continue
-
-            question = item.get("question", "")
-            
-            paragraphs = [ctx["text"] for ctx in item["context"]]
-            if len(paragraphs) <= 3:
-                continue
-
-            additional_paragraphs = list(all_paragraphs - set(paragraphs))
-            sampled_paragraphs = random.sample(additional_paragraphs, min(10, len(additional_paragraphs)))
-            paragraphs += sampled_paragraphs
-            random.shuffle(paragraphs)
-            
-            answer = item.get("annotated_para", "")
-            answer_index = next((i for i, p in enumerate(paragraphs) if answer in p), -1)
-
-            entry = {
-                "question": question,
-                "paragraphs": paragraphs,
-                "answer": [answer_index] if answer_index >= 0 else [0]
-            }
-            
-            processed_data.append(entry)
-
-        with output_file.open("w", encoding="utf-8") as outfile:
-            json.dump(processed_data, outfile, indent=2, ensure_ascii=False)
-
-        if dataset_name.lower() == "menat_qa":
-            print(f"Processed dataset saved to: {output_file}")
-        
-        else:
-            print(f"Processed filtered dataset saved to: {output_file}")
+        create_menat_qa_benchmark(dataset_name)
 
     elif dataset_name.lower().startswith("ts_retriever"):
         create_ts_retriever_benchmark()
@@ -96,12 +20,6 @@ def create_evaluation_dataset(dataset_name):
         
     else:
         print(f"Dataset '{dataset_name}' is not supported.")
-
-def fetch_random_paragraphs(pargraphs: List[str], num_paragraphs: int) -> List[str]:
-    if len(pargraphs) <= num_paragraphs:
-        return pargraphs
-    
-    return random.sample(pargraphs, num_paragraphs)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process evaluation dataset.")
---
+++ temporal_embeddings/evaluation/benchmarks/menat_qa.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+import json
+import random
+from typing import List, Dict
+
+def create_menat_qa_benchmark(dataset_name: str) -> None:
+    main_folder: Path = Path("data/evaluation/menat_qa")
+    input_path: Path = main_folder / Path("MenatQA.json")
+    
+    if dataset_name.lower() == "menat_qa":
+        output_file: Path = main_folder / "processed_menat_qa.json"
+    
+    elif dataset_name.lower() == "menat_qa_granularity":
+        output_file: Path = main_folder / "processed_menat_qa_granularity.json"
+    
+    elif dataset_name.lower() == "menat_qa_counterfactual":
+        output_file: Path = main_folder / "processed_menat_qa_counterfactual.json"
+    
+    elif dataset_name.lower() == "menat_qa_expand":
+        output_file: Path = main_folder / "processed_menat_qa_expand.json"
+    
+    elif dataset_name.lower() == "menat_qa_narrow":
+        output_file: Path = main_folder / "processed_menat_qa_narrow.json"
+    
+    else:
+        raise ValueError("Unsupported dataset name.")
+
+    main_folder.mkdir(parents=True, exist_ok=True)
+
+    with input_path.open("r", encoding="utf-8") as infile:
+        menat_data: List[Dict] = json.load(infile)
+
+    processed_data: List[Dict] = []
+    
+    for item in menat_data:
+        if dataset_name.lower() == "menat_qa_granularity" and item.get("type") != "granularity":
+            continue
+        
+        if dataset_name.lower() == "menat_qa_counterfactual" and item.get("type") != "counterfactual":
+            continue
+        
+        if dataset_name.lower() == "menat_qa_expand" and item.get("type") != "expand":
+            continue
+        
+        if dataset_name.lower() == "menat_qa_narrow" and item.get("type") != "narrow":
+            continue
+
+        question: str = item.get("question", "")
+        
+        paragraphs: List[str] = [ctx["text"] for ctx in item["context"]]
+        if len(paragraphs) <= 3:
+            continue
+        
+        answer: str = item.get("annotated_para", "")
+        answer_index: int = next((i for i, p in enumerate(paragraphs) if answer in p), -1)
+
+        entry = {
+            "question": question,
+            "paragraphs": paragraphs,
+            "answer": [answer_index] if answer_index >= 0 else [0]
+        }
+        
+        processed_data.append(entry)
+
+    with output_file.open("w", encoding="utf-8") as outfile:
+        json.dump(processed_data, outfile, indent=2, ensure_ascii=False)
+
+    if dataset_name.lower() == "menat_qa":
+        print(f"Processed dataset saved to: {output_file}")
+    
+    else:
+        print(f"Processed filtered dataset saved to: {output_file}")
