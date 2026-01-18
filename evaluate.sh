cd2896eb5457cf461094bc474849f52bbe98d368
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-18 12:56:07 +0100

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=23:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --nodelist=n53
+#SBATCH --nodelist=n52
 
 source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
@@ -9,9 +9,9 @@ conda activate train-env
 
 BENCHMARKS=("time_sensitive_qa")
 EXTERNAL_MODELS=("intfloat/e5-base-v2" "all-mpnet-base-v2" "BAAI/bge-large-en-v1.5" "salesforce")
-NUM_NEGATIVE_SAMPLES=(-1)
+NUM_NEGATIVE_SAMPLES=(0)
 ALPHA_VALUES=($(seq 0.05 0.05 0.95))
-eval_id="Final Evaluation"
+eval_id="New architecture: 50M"
 
 echo "##########################################"
 echo "### EXTERNAL MODELS ONLY (no alpha) ###"
@@ -24,7 +24,7 @@ for benchmark in "${BENCHMARKS[@]}"; do
     for num_neg in "${NUM_NEGATIVE_SAMPLES[@]}"; do
         echo "Num Negative Samples: $num_neg"
 
-        python3 evaluate.py --model_name=all-minilm-l6-v2 --model_path=output/trained_models/model_sentence-transformers_all-MiniLM-L6-v2_2025-07-25_23-47-04.pth --batch_size=128 --max_seq_len=512 --benchmark=$benchmark --eval_id="$eval_id" --top_k=5 --metric=all --num_negative_samples=$num_neg
+        python3 evaluate.py --model_name=all-minilm-l6-v2 --model_path=output/trained_models/model_sentence-transformers_all-MiniLM-L6-v2_2026-01-18_02-11-00.pth --batch_size=128 --max_seq_len=512 --benchmark=$benchmark --eval_id="$eval_id" --top_k=5 --metric=all --num_negative_samples=$num_neg
         
         for external_model in "${EXTERNAL_MODELS[@]}"; do
             echo "External Model: $external_model"
@@ -47,7 +47,7 @@ for alpha in "${ALPHA_VALUES[@]}"; do
             
             for external_model in "${EXTERNAL_MODELS[@]}"; do
                 echo "External Model: $external_model (with alpha)"
-                python3 evaluate.py --model_name=all-minilm-l6-v2-full --external_model_name=$external_model --model_path=output/trained_models/model_sentence-transformers_all-MiniLM-L6-v2_2025-07-25_23-47-04.pth --batch_size=128 --max_seq_len=512 --benchmark=$benchmark --eval_id="$eval_id" --top_k=5 --metric=all --alpha=$alpha --num_negative_samples=$num_neg
+                python3 evaluate.py --model_name=all-minilm-l6-v2-full --external_model_name=$external_model --model_path=output/trained_models/model_sentence-transformers_all-MiniLM-L6-v2_2026-01-18_02-11-00.pth --batch_size=128 --max_seq_len=512 --benchmark=$benchmark --eval_id="$eval_id" --top_k=5 --metric=all --alpha=$alpha --num_negative_samples=$num_neg
             done
         done
     done
