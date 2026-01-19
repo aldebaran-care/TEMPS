f0e1d49237e6973137124084bae230acb94dbd87
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-19 11:50:21 +0100

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -7,7 +7,7 @@ source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
 conda activate train-env
 
-BENCHMARKS=("time_sensitive_qa")
+BENCHMARKS=("menat_qa")
 EXTERNAL_MODELS=("intfloat/e5-base-v2" "all-mpnet-base-v2" "BAAI/bge-large-en-v1.5" "salesforce")
 NUM_NEGATIVE_SAMPLES=(0)
 ALPHA_VALUES=($(seq 0.05 0.05 0.95))
