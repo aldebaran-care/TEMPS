992c783856c56f8f12fa3699865233633da2eeb3
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-19 02:27:26 +0100

    Update train.sh

--- train.sh
+++ train.sh
@@ -8,5 +8,5 @@
 source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
 conda activate train-env
-torchrun --nproc_per_node=3 --nnodes=1 train.py --data_fraction=1 --epochs=1 --batch_size=128 --model_name=sentence-transformers/all-MiniLM-L6-v2 --input_file_path data/new_training_dataset/training_datasets/training_data_chunk_1_20.csv --continue_training --model_path output/trained_models/checkpoint_sentence-transformers_all-MiniLM-L6-v2_2026-01-18_02-33-03_final.pth
+torchrun --nproc_per_node=3 --nnodes=1 train.py --data_fraction=1 --epochs=1 --batch_size=128 --model_name=sentence-transformers/all-MiniLM-L6-v2 --input_file_path data/new_training_dataset/training_datasets/merged_training_data.csv --continue_training --model_path output/trained_models/checkpoint_sentence-transformers_all-MiniLM-L6-v2_2026-01-18_19-41-08_final.pth
 conda deactivate
