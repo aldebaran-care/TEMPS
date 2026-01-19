88f3ed2b0bf04c16760217b2dcf43cd97240dffa
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-19 16:06:20 +0100

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -7,9 +7,9 @@ source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
 conda activate train-env
 
-BENCHMARKS=("temp_rag_eval")
+BENCHMARKS=("menat_qa_narrow")
 EXTERNAL_MODELS=("intfloat/e5-base-v2" "all-mpnet-base-v2" "BAAI/bge-large-en-v1.5" "salesforce")
-NUM_NEGATIVE_SAMPLES=(20)
+NUM_NEGATIVE_SAMPLES=(0)
 ALPHA_VALUES=($(seq 0.05 0.05 0.95))
 EVAL_ID="New architecture: 50M"
 MODEL_PATH="output/trained_models/model_sentence-transformers_all-MiniLM-L6-v2_2026-01-19_02-33-51.pth"
