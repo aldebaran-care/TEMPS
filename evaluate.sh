7bc6c029dfe7d54c2f53d318f3d68277d1330c3a
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-19 19:16:14 +0100

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -10,7 +10,7 @@ conda activate train-env
 BENCHMARKS=("data/evaluation/my_benchmark/benchmark.json")
 EXTERNAL_MODELS=("intfloat/e5-base-v2" "all-mpnet-base-v2" "BAAI/bge-large-en-v1.5" "salesforce")
 NUM_NEGATIVE_SAMPLES=(-1)
-ALPHA_VALUES=($(seq -0.05 -0.05 -0.95))
+ALPHA_VALUES=($(seq 0.05 0.05 0.95))
 EVAL_ID="New architecture: 50M"
 MODEL_PATH="output/trained_models/model_sentence-transformers_all-MiniLM-L6-v2_2026-01-19_02-33-51.pth"
 BATCH_SIZE=128
