726765cd63dabe22758df31bca6080566c1d0993
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-20 12:30:28 +0100

    Split the paragraphs in ts_retriever

--- temporal_embeddings/evaluation/benchmarks/ts_retriever.py
+++ temporal_embeddings/evaluation/benchmarks/ts_retriever.py
@@ -9,36 +9,31 @@ def create_ts_retriever_benchmark() -> None:
     output_path: Path = base / "processed_ts_retriever.json"
 
     with query_path.open("r", encoding="utf-8") as f:
-        questions: List[Dict] = [{"query": q["query"], "positive_text": [pt[:400] for pt in q["positive_text"]]} for q in json.load(f)]
+        questions: List[Dict] = [{"query": q["query"], "positive_text": [ps for pt in q["positive_text"] for ps in pt.split("\n") if len(ps.strip().split()) > 3]} for q in json.load(f)]
 
     with doc_path.open("r", encoding="utf-8") as f:
-        paragraphs = [p[:400] for p in json.load(f)]
+        paragraphs: List[str] = list(set([s for p in json.load(f) for s in p.split("\n") if len(s.strip().split()) > 3]))
 
     output: List[Dict] = []
 
-    for _, q in enumerate(questions):
+    for i, q in enumerate(questions):
         answer_idx: List[int] = []
         
-        for positive_text in q["positive_text"]:
+        for positive_sentence in q["positive_text"]:
             try:
-                index: int = paragraphs.index(positive_text)
+                index: int = paragraphs.index(positive_sentence)
             
             except ValueError:
-                print(f"Warning: Positive text not found in paragraphs: {positive_text[:50]}...")
-                paragraphs.append(positive_text)
+                print(f"Warning: Positive sentence not found in paragraphs: {positive_sentence[:50]}...")
+                paragraphs.append(positive_sentence)
                 index = len(paragraphs) - 1
-                
+            
             answer_idx.append(index)
-
-        answer_idx = []
-        
-        for positive_text in q["positive_text"]:
-            answer_idx.append(paragraphs.index(positive_text))
 
         entry = {
             "question": q["query"],
-            "paragraphs": paragraphs,
-            "answer": answer_idx,
+            "paragraphs": paragraphs if i == 0 else q["positive_text"],
+            "answer": answer_idx if i == 0 else list(range(len(q["positive_text"]))),
         }
 
         output.append(entry)
