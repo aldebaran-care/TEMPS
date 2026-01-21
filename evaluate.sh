9226947d5e47739399e1e1b4ffd2d9187e220853
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-21 05:03:16 +0100

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=23:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --nodelist=n53
+#SBATCH --nodelist=n54
 
 source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
