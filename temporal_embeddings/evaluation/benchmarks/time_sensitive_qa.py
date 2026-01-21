c9f915d63f6cecdc6e862e128516026303f643f1
Mourad Hassani <mouradhassani.9101@gmail.com>
2026-01-21 04:44:13 +0100

    Update time_sensitive_qa.py

--- temporal_embeddings/evaluation/benchmarks/time_sensitive_qa.py
+++ temporal_embeddings/evaluation/benchmarks/time_sensitive_qa.py
@@ -27,7 +27,7 @@ def create_time_sensitive_qa_benchmark(add_negative_samples: bool) -> None:
             answers = q_pair[1]
             
             for ans in answers:
-                para_idx = ans.get("para", 0)
+                para_idx = ans["para"]
                 
                 entry = {
                     "question": question_text,
